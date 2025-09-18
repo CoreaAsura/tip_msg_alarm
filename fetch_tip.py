7995039b8ca7c712a8ac62f99df5e8454e06caae
CoreaAsura <mulgogic7d@gmail.com>
2025-09-18 22:23:29 +0900

    Update fetch_tip.py

--- fetch_tip.py
+++ fetch_tip.py
@@ -1,9 +1,13 @@
 import requests
+import os
 
 def fetch_tip_messages(email, password, save_path):
     login_url = "https://www.space-track.org/ajaxauth/login"
     query_url = "https://www.space-track.org/basicspacedata/query/class/tip/orderby/MSG_EPOCH desc/format/csv"
     credentials = {"identity": email, "password": password}
+
+    # ✅ 폴더가 없으면 생성
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
 
     with requests.Session() as session:
         session.post(login_url, data=credentials)
